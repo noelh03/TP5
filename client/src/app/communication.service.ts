f87120fe02dac3a487f9199543224b1c9a9db9e6
Niang-04 <moustaphang04@gmail.com>
2024-04-10 14:23:42 -0400

    Update communication service suite a ajout component especeoiseau

--- client/src/app/communication.service.ts
+++ client/src/app/communication.service.ts
@@ -3,10 +3,7 @@ import { Injectable } from "@angular/core";
 // tslint:disable-next-line:ordered-imports
 import { of, Observable, Subject } from "rxjs";
 import { catchError } from "rxjs/operators";
-import { Hotel } from "../../../common/tables/Hotel";
-import { Room } from "../../../common/tables/Room";
-import { HotelPK } from "../../../common/tables/HotelPK";
-import { Guest } from "../../../common/tables/Guest";
+import { Especeoiseau } from "../../../common/tables/Especeoiseau";
 
 @Injectable()
 export class CommunicationService {
@@ -23,71 +20,33 @@ export class CommunicationService {
     this._listners.next(filterBy);
   }
 
-  public getHotels(): Observable<Hotel[]> {
+  public getEspecesOiseaux(): Observable<Especeoiseau[]> {
     return this.http
-      .get<Hotel[]>(this.BASE_URL + "/hotels")
-      .pipe(catchError(this.handleError<Hotel[]>("getHotels")));
+      .get<Especeoiseau[]>(this.BASE_URL + "/birds")
+      .pipe(catchError(this.handleError<Especeoiseau[]>("getEspecesOiseaux")));
   }
 
-  public insertHotel(hotel: Hotel): Observable<number> {
+  public insertEspeceOiseau(espece: Especeoiseau): Observable<number> {
     return this.http
-      .post<number>(this.BASE_URL + "/hotels/insert", hotel)
-      .pipe(catchError(this.handleError<number>("insertHotel")));
+      .post<number>(this.BASE_URL + "/birds/insert", espece)
+      .pipe(catchError(this.handleError<number>("insertEspeceOiseau")));
   }
 
-  public updateHotel(hotel: Hotel): Observable<number> {
+  public updateEspeceOiseau(espece: Especeoiseau): Observable<number> {
     return this.http
-      .put<number>(this.BASE_URL + "/hotels/update", hotel)
-      .pipe(catchError(this.handleError<number>("updateHotel")));
+      .put<number>(this.BASE_URL + "/birds/update", espece)
+      .pipe(catchError(this.handleError<number>("updateEspeceOiseau")));
   }
 
-  public deleteHotel(hotelNb: string): Observable<number> {
+  public deleteEspeceOiseau(nomscientifique: string): Observable<number> {
     return this.http
-      .post<number>(this.BASE_URL + "/hotels/delete/" + hotelNb, {})
-      .pipe(catchError(this.handleError<number>("deleteHotel")));
+      .delete<number>(this.BASE_URL + `/birds/delete/${nomscientifique}`)
+      .pipe(catchError(this.handleError<number>("deleteEspeceOiseau")));
   }
 
-  public getHotelPKs(): Observable<HotelPK[]> {
-    return this.http
-      .get<HotelPK[]>(this.BASE_URL + "/hotels/hotelNb")
-      .pipe(catchError(this.handleError<HotelPK[]>("getHotelPKs")));
-  }
-
-  public getRooms(hotelNb: string): Observable<Room[]> {
-    return this.http
-      .get<Room[]>(this.BASE_URL + `/rooms?hotelNb=${hotelNb}`)
-      .pipe(catchError(this.handleError<Room[]>("getRooms")));
-  }
-
-  public insertRoom(room: Room): Observable<number> {
-    return this.http
-      .post<number>(this.BASE_URL + "/rooms/insert", room)
-      .pipe(catchError(this.handleError<number>("inserHotel")));
-  }
-
-  public updateRoom(room: Room): Observable<number> {
-    return this.http
-      .put<number>(this.BASE_URL + "/rooms/update", room)
-      .pipe(catchError(this.handleError<number>("updateRoom")));
-  }
-
-  public deleteRoom(hotelNb: string, roomNb: string): Observable<number> {
-    return this.http
-      .post<number>(this.BASE_URL + `/rooms/delete/${hotelNb}/${roomNb}`, {})
-      .pipe(catchError(this.handleError<number>("deleteRoom")));
-  }
-
-  public getGuests(hotelNb: string, roomNb: string): Observable<Guest[]> {
-    return this.http
-      .get<Guest[]>(this.BASE_URL + `/guests/${hotelNb}/${roomNb}`)
-      .pipe(catchError(this.handleError<Guest[]>("getGuests")));
-  }
-
-  private handleError<T>(
-    request: string,
-    result?: T
-  ): (error: Error) => Observable<T> {
+  private handleError<T>(request: string, result?: T): (error: Error) => Observable<T> {
     return (error: Error): Observable<T> => {
+      console.error("Error occurred:", error);
       return of(result as T);
     };
   }
