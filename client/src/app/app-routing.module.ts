afae7fcc4eca3b4fee72ff9aa844f19b2325f30c
Niang-04 <moustaphang04@gmail.com>
2024-04-10 14:23:04 -0400

    update app-routing suite a ajout component especeoiseau

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from "./app.component";
-import { HotelComponent } from "./hotel/hotel.component";
-import { RoomComponent } from "./room/room.component";
-import { GuestComponent } from "./guest/guest.component";
+import { EspeceOiseauComponent } from "./especeoiseau/especeoiseau.component";
 
 const routes: Routes = [
   { path: "app", component: AppComponent },
-  { path: "rooms", component: RoomComponent },
-  { path: "hotels", component: HotelComponent },
-  { path: "guests", component: GuestComponent },
+  { path: "birds", component: EspeceOiseauComponent },
+  
 ];
 
 @NgModule({
