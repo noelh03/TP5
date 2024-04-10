9aa688fdfcd66a76deed3c067f9ca606a98ab1a6
Niang-04 <moustaphang04@gmail.com>
2024-04-10 11:01:23 -0400

    Ajout statut espece

--- common/tables/Especeoiseau.ts
+++ common/tables/Especeoiseau.ts
@@ -1,6 +1,13 @@
+export enum Statut {
+    Vulnerable = 'Vulnérable',
+    Préoccupation_mineure = 'Préoccupation mineure',
+    Non_menacee = 'Non menacée'
+}
+
+
 export interface Especeoiseau {
     nomscientifique: string;
     nomcommun: string;
-    statutspeces: string;
+    statutspeces: Statut;
     nomscientifiquecomsommer: string;
 }
